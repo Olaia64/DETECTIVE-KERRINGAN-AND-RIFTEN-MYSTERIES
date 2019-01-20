838d87f7925f73fe2461194ac2cd79f7e721d0b6
unaitb2 <43841708+unaitb2@users.noreply.github.com>
2019-01-20 12:44:53 +0100

    Update Jokoa.c

--- Detective Ellie and Riften's mysteries/Jokoa/02simpleGame/game02/Jokoa.c
+++ Detective Ellie and Riften's mysteries/Jokoa/02simpleGame/game02/Jokoa.c
@@ -50,210 +50,207 @@ EGOERA jokatu(void)
 			menu = mainMenu();//Menura joateko funtzioa
 			jokoahasi = 1;
 		}
-			pantailaGarbitu();
-			if (hasierakoElkarrizketa == 0) {
+		pantailaGarbitu();
+		if (hasierakoElkarrizketa == 0) {
 				hasierakoElkarrizketa =lehenElkarrizketa();
 			}
-			irudiaMugitu(jokalaria.id, jokalaria.pos.x, jokalaria.pos.y);
-			irudiakMarraztu();
+		irudiaMugitu(jokalaria.id, jokalaria.pos.x, jokalaria.pos.y);
+		irudiakMarraztu();
+		pantailaBerriztu();
+		jokoarenAmaiera =mugimendua(puzzle6,kulto);
+		if (jokoarenAmaiera == 1) {//Azken puzzlea bete bada score-a jartzeko
+			probafinal = 0;
+			menu = 1;
+			printeatuScore = 1;
+		}
+		if (printeatuScore == 1)//Puzzleen, elkarrizketen eta noteen batura{
+			score = score + scorepuzzle;
+			printeatuScore = 0;
+			score = score + scoreerreka;
+			pantailaGarbitu();
+			scorepuzzle = 0;
+			scoreerreka = 0;
+			textuaIdatzi(100, 200, SCORE_TEXTUA);
+			sprintf(buffer, "%d", score);
+			textuaIdatzi(225, 200, buffer);
 			pantailaBerriztu();
-			jokoarenAmaiera =mugimendua(puzzle6,kulto);
-			if (jokoarenAmaiera == 1) {//Azken puzzlea bete bada score-a jartzeko
-				probafinal = 0;
+			Sleep(5000);
+			jokoahasi = -1;
+			}
+		filak = jokalaria.pos.y / 10;//jokalariaren posizioa matrizera pasatzeko kalkuloa y ardatzan
+		zutabeak = jokalaria.pos.x / 10;//jokalariaren posizioa matrizera pasatzeko kalkuloa x ardatzan
+		if (mapa[filak][zutabeak] == 1) {//ezkerreko kolisioa egin
+			mugimenduaezkerra = 0;
+		}
+		if (mapa[filak][zutabeak] == 2) {//goiko kolisioa egin
+			up = 0;
+		}
+		if (mapa[filak][zutabeak] == 3) {//beheko kolisioa egin
+			mugimenduabehean = 0;
+		}
+		if (mapa[filak][zutabeak] == 4) {//eskuineko kolisioa egin
+			mugimenduaeskuina = 0;
+		}
+		if (mapa[filak][zutabeak] == 5) {//ezker-goiko kolisioa egin
+			mugimenduaezkerra = 0;
+			up = 0;
+		}
+		if (mapa[filak][zutabeak] == 6) {//ezker-beheko kolisioa egin
+			mugimenduaezkerra = 0;
+			mugimenduabehean = 0;
+		}
+		if (mapa[filak][zutabeak] == 7) {//goi-eskuineko kolisioa egin
+			mugimenduaeskuina = 0;
+			up = 0;
+		}
+		if (mapa[filak][zutabeak] == 8) {//behe-eskuineko kolisioa egin
+			mugimenduaeskuina = 0;
+			mugimenduabehean = 0;
+		}
+		if (mapa[filak][zutabeak] == 16) {//hilketaren textua jarri
+			hilketaTextua();
+			score = score + 5;
+		}
+		if (mapa[filak][zutabeak] == 15) {//hilketa mapatik ateratzea
+			hilketatikMaparaAtera();
+		}
+		if (mapa[filak][zutabeak] == 13) {//lehenengo puzzlearen kodigoa
+			mugimeduaEllieHilketa = lehenengoPuzzlea();
+			if (mugimeduaEllieHilketa == 1) {//irabazi baduzu
+				puzzle2 = 0;
+				puzzleMaltzurra = 0;
+				puzzleHilketa = 0;
+				score = score + 25;
+				audioInit();
+				loadTheMusic(JOKOA_SOUND_MAPA);
+				playMusic();
+			}
+			else if (mugimeduaEllieHilketa == 2) {//berriro jokatu sakatzean
+				score = score - 50;
+			}
+			else {//bueltatu menura sakatzean
+				score = 0;
+				jokoahasi = 0;
 				menu = 1;
-				printeatuScore = 1;
+				hasierakoElkarrizketa = 0;
 			}
-			if (printeatuScore == 1)//Puzzleen, elkarrizketen eta noteen batura
-			{
-				score = score + scorepuzzle;
-				printeatuScore = 0;
-				score = score + scoreerreka;
-				pantailaGarbitu();
-				scorepuzzle = 0;
-				scoreerreka = 0;
-				textuaIdatzi(100, 200, SCORE_TEXTUA);
-				sprintf(buffer, "%d", score);
-				textuaIdatzi(225, 200, buffer);
-				pantailaBerriztu();
-				Sleep(5000);
-				jokoahasi = -1;
+		}
+		if (mapa[filak][zutabeak] == 24) {//bulegoko elkarrizketa
+			bulegoTextSortu();
+			elkarrizketa = 1;
+		}
+		if (mapa[filak][zutabeak] == 23) {//elkarrizketaren konprobaketa
+			if (elkarrizketa == 0) {//elkarrizketa ez badago eginda
+				elkarrizketaEzDagoEginda();
 			}
-			filak = jokalaria.pos.y / 10;//jokalariaren posizioa matrizera pasatzeko kalkuloa y ardatzan
-			zutabeak = jokalaria.pos.x / 10;//jokalariaren posizioa matrizera pasatzeko kalkuloa x ardatzan
-			if (mapa[filak][zutabeak] == 1) {//ezkerreko kolisioa egin
-				mugimenduaezkerra = 0;
+			if (elkarrizketa == 1) {//elkarrizketa eginda dago
+				elkarrizketaEgindaDago();
 			}
-			if (mapa[filak][zutabeak] == 2) {//goiko kolisioa egin
-				up = 0;
-			}
-			if (mapa[filak][zutabeak] == 3) {//beheko kolisioa egin
-				mugimenduabehean = 0;
-			}
-			if (mapa[filak][zutabeak] == 4) {//eskuineko kolisioa egin
-				mugimenduaeskuina = 0;
-			}
-			if (mapa[filak][zutabeak] == 5) {//ezker-goiko kolisioa egin
-				mugimenduaezkerra = 0;
-				up = 0;
-			}
-			if (mapa[filak][zutabeak] == 6) {//ezker-beheko kolisioa egin
-				mugimenduaezkerra = 0;
-				mugimenduabehean = 0;
-			}
-			if (mapa[filak][zutabeak] == 7) {//goi-eskuineko kolisioa egin
-				mugimenduaeskuina = 0;
-				up = 0;
-			}
-			if (mapa[filak][zutabeak] == 8) {//behe-eskuineko kolisioa egin
-				mugimenduaeskuina = 0;
-				mugimenduabehean = 0;
-			}
-			if (mapa[filak][zutabeak] == 16) {//hilketaren textua jarri
-				hilketaTextua();
+		}
+		if (mapa[filak][zutabeak] == 31) {//bulegoaren irudia sortu
+			bulegokoIrudiaSortu();
+		}
+		if (mapa[filak][zutabeak] == 32) {//hilketaren irudia sortu
+			hilketaIrudiaSortu();
+		}
+		if (mapa[filak][zutabeak] == 34) {//erreka puzzlea jarri
+			scoreerreka=errekaPuzzlea();
+			mapaglobal.id = irabaztean();
+			mapaglobal.id = JOKOA_Mapa1IrudiaSortu();
+			kopiatuMapaNagusia1();
+			jokalaria.id = JOKOA_jokalariaAurreraIrudiaSortu();
+		}
+		if (mapa[filak][zutabeak] == 40) {//nota1
+			if (nota1 == 0) {
+				notaImg();
 				score = score + 5;
+				nota1 = 1;
 			}
-			if (mapa[filak][zutabeak] == 15) {//hilketa mapatik ateratzea
-				hilketatikMaparaAtera();
+		}
+		if (mapa[filak][zutabeak] == 41) {//nota2
+			if (nota2 == 0) {
+				notaImg();
+				score = score + 5;
+				nota2 = 1;
 			}
-			if (mapa[filak][zutabeak] == 13) {//lehenengo puzzlearen kodigoa
-				mugimeduaEllieHilketa = lehenengoPuzzlea();
-				if (mugimeduaEllieHilketa == 1) {//irabazi baduzu
-					puzzle2 = 0;
-					puzzleMaltzurra = 0;
-					puzzleHilketa = 0;
-					score = score + 25;
-					audioInit();
-					loadTheMusic(JOKOA_SOUND_MAPA);
-					playMusic();
-				}
-				else if (mugimeduaEllieHilketa == 2) {//berriro jokatu sakatzean
-					score = score - 50;
-				}
-				else {//bueltatu menura sakatzean
-					score = 0;
-					jokoahasi = 0;
-					hasi = 0;
-					menu = 1;
-					hasierakoElkarrizketa = 0;
-				}
+		}
+		if (mapa[filak][zutabeak] == 42) {//nota3
+			if (nota3 == 0) {
+				notaImg();
+				score = score + 5;
+				nota3 = 1;
 			}
-			if (mapa[filak][zutabeak] == 24) {//bulegoko elkarrizketa
-				bulegoTextSortu();
-				elkarrizketa = 1;
+		}
+		if (mapa[filak][zutabeak] == 43) {//nota4
+			if (nota4 == 0) {
+				notaImg();
+				score = score + 5;
+				nota4 = 1;
 			}
-			if (mapa[filak][zutabeak] == 23) {//elkarrizketaren konprobaketa
-				if (elkarrizketa == 0) {//elkarrizketa ez badago eginda
-					elkarrizketaEzDagoEginda();
-				}
-				if (elkarrizketa == 1) {//elkarrizketa eginda dago
-					elkarrizketaEgindaDago();
-				}
+		}
+		if (mapa[filak][zutabeak] == 44 && ebentu == TECLA_e) {//nota5
+			if (nota5 == 0) {
+				notaImg();
+				score = score + 5;
+				nota5 = 1;
 			}
-			if (mapa[filak][zutabeak] == 31) {//bulegoaren irudia sortu
-				bulegokoIrudiaSortu();
+		}
+		if (mapa[filak][zutabeak] == 45 && ebentu == TECLA_e) {//nota6
+			if (nota6 == 0) {
+				notaImg();
+				score = score + 5;
+				nota6 = 1;
 			}
-			if (mapa[filak][zutabeak] == 32) {//hilketaren irudia sortu
-				hilketaIrudiaSortu();
+		}
+		if (mapa[filak][zutabeak] == 60&&jokoahasi==1) {//kultoaren puzzlea
+			kultoa = puzzle3(arropa);
+			if (kultoa == 1) {//ondo egin bada
+				score = score + 25;
 			}
-			if (mapa[filak][zutabeak] == 34) {//erreka puzzlea jarri
-				scoreerreka=errekaPuzzlea();
-				mapaglobal.id = irabaztean();
-				mapaglobal.id = JOKOA_Mapa1IrudiaSortu();
-				kopiatuMapaNagusia1();
-				jokalaria.id = JOKOA_jokalariaAurreraIrudiaSortu();
+			else if (kultoa == 2) {//jokatu berriro
+				score = score - 50;
 			}
-			if (mapa[filak][zutabeak] == 40) {//nota1
-				if (nota1 == 0) {
-					notaImg();
-					score = score + 5;
-					nota1 = 1;
-				}
-			}
-			if (mapa[filak][zutabeak] == 41) {//nota2
-				if (nota2 == 0) {
-					notaImg();
-					score = score + 5;
-					nota2 = 1;
-				}
-			}
-			if (mapa[filak][zutabeak] == 42) {//nota3
-				if (nota3 == 0) {
-					notaImg();
-					score = score + 5;
-					nota3 = 1;
-				}
-			}
-			if (mapa[filak][zutabeak] == 43) {//nota4
-				if (nota4 == 0) {
-					notaImg();
-					score = score + 5;
-					nota4 = 1;
-				}
-			}
-			if (mapa[filak][zutabeak] == 44 && ebentu == TECLA_e) {//nota5
-				if (nota5 == 0) {
-					notaImg();
-					score = score + 5;
-					nota5 = 1;
-				}
-			}
-			if (mapa[filak][zutabeak] == 45 && ebentu == TECLA_e) {//nota6
-				if (nota6 == 0) {
-					notaImg();
-					score = score + 5;
-					nota6 = 1;
-				}
-			}
-			if (mapa[filak][zutabeak] == 60&&jokoahasi==1) {//kultoaren puzzlea
-				kultoa = puzzle3(arropa);
-				if (kultoa == 1) {//ondo egin bada
-					score = score + 25;
-				}
-				else if (kultoa == 2) {//jokatu berriro
-					score = score - 50;
-				}
-				else if(kulto==3){//bueltatu menura
-					hasierakoElkarrizketa = 0;
-					jokoahasi = 0;
-					hasi = 0;
-					menu = 1;
-					kulto = 0;
-					score = 0;
-				}
-			}
-			if (mapa[filak][zutabeak] == 61) {//kulto mapatik ateratzeko
-				kultotikMaparaAtera();
+			else if(kulto==3){//bueltatu menura
+				hasierakoElkarrizketa = 0;
+				jokoahasi = 0;
+				menu = 1;
 				kulto = 0;
-				probafinal = 1;
-				arropaJantzitaEgonDa = 1;
+				score = 0;
 			}
-			if (mapa[filak][zutabeak] == 62) {//kultoaren elkarrizketa 1
-				kultotxt1();
-				score = score + 5;
+		}
+		if (mapa[filak][zutabeak] == 61) {//kulto mapatik ateratzeko
+			kultotikMaparaAtera();
+			kulto = 0;
+			probafinal = 1;
+			arropaJantzitaEgonDa = 1;
+		}
+		if (mapa[filak][zutabeak] == 62) {//kultoaren elkarrizketa 1
+			kultotxt1();
+			score = score + 5;
+		}
+		if (mapa[filak][zutabeak] == 63) {//kultoaren elkarrizketa 2
+			kultotxt2();
+			score = score + 5;
+		}
+		if (mapa[filak][zutabeak] == 64) {//kultoaren elkarrizketa 3
+			kultotxt3();
+			score = score + 5;
+		}
+		if (mapa[filak][zutabeak] == 65) {//kultoaren elkarrizketa 4
+			kultotxt4();
+		}
+		if (mapa[filak][zutabeak] == 99&&arropaJantzitaEgonDa==0) {//arropa jarri
+			arropaJarri();
+			kulto = 1;
+			arropa = 1;
+		}
+		if (mapa[filak][zutabeak] == 36 && probafinal == 1) {//puzzle finala jarri
+			mapaFinala();
+			puzzle6 = 1;
 			}
-			if (mapa[filak][zutabeak] == 63) {//kultoaren elkarrizketa 2
-				kultotxt2();
-				score = score + 5;
-			}
-			if (mapa[filak][zutabeak] == 64) {//kultoaren elkarrizketa 3
-				kultotxt3();
-				score = score + 5;
-			}
-			if (mapa[filak][zutabeak] == 65) {//kultoaren elkarrizketa 4
-				kultotxt4();
-			}
-			if (mapa[filak][zutabeak] == 99&&arropaJantzitaEgonDa==0) {//arropa jarri
-				arropaJarri();
-				kulto = 1;
-				arropa = 1;
-			}
-			if (mapa[filak][zutabeak] == 36 && probafinal == 1) {//puzzle finala jarri
-				mapaFinala();
-				puzzle6 = 1;
-			}
-			if (ebentu == TECLA_p) {//pausa menua
-				pausa=pausaMenua();
-			}
+		if (ebentu == TECLA_p) {//pausa menua
+			pausa=pausaMenua();
+		}
 
 		if (jokoahasi == -1) {//amaireako menua
 			amaiera = amaieraMenua();
