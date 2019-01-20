1e51ab6941dab289fd2adca4348d1da26b8ddba7
Olaia64 <46557197+Olaia64@users.noreply.github.com>
2019-01-20 18:44:51 +0100

    Update game02.h

--- Detective Ellie and Riften's mysteries/Jokoa/02simpleGame/game02/game02.h
+++ Detective Ellie and Riften's mysteries/Jokoa/02simpleGame/game02/game02.h
@@ -10,98 +10,67 @@ int JOKOA_jokalariaAurreraIrudiaSortu();
 int JOKOA_jokalariaEskuineraIrudiaSortu();
 int JOKOA_jokalariaEzkerreraIrudiaSortu();
 int JOKOA_jokalariaAtzeraIrudiaSortu();
+int JOKOA_jokalariaKEskuineraIrudiaSortu();
+int JOKOA_jokalariaKEzkerreraIrudiaSortu();
+int JOKOA_jokalariaKAurreraIrudiaSortu();
+int JOKOA_jokalariaKAtzeraIrudiaSortu();
+int mugimendua(int puzzle6, int kulto);
+
+//menua
+int mainMenu();
 int JOKOA_menuIrudiaSortu();
 int JOKOA_kontrolakIrudiaSortu();
 int JOKOA_menuIrtenIrudiaSortu();
 int JOKOA_kontrolakAzalpenaIrudiaSortu();
 int JOKOA_puntuaketaIrudiaSortu();
-int JOKOA_jokalariaKEskuineraIrudiaSortu();
-int JOKOA_jokalariaKEzkerreraIrudiaSortu();
-int JOKOA_jokalariaKAurreraIrudiaSortu();
-int JOKOA_jokalariaKAtzeraIrudiaSortu();
+int JOKOA_galduIrudiaSortu();
+int JOKOA_galdu1IrudiaSortu();
+int menuJarraitu();
+int menuIrten2();
+int menuBueltatu();
+int menuIrten();
+int pausaMenua();
+int amaieraMenua();
+
+//Elkarrizketak
 int JOKOA_hilketatxt3IrudiaSortu();
 int JOKOA_kultoatxt4IrudiaSortu();
 int JOKOA_kultoatxt5IrudiaSortu();
 int JOKOA_kultoatxt6IrudiaSortu();
 int JOKOA_ArropatxtIrudiaSortu();
-int PUZZLE6_irudiaBistaratu();//puzzle4
-int JOKOA_ErrekaIrudiaSortu();
 int JOKOA_kultoatxtIrudiaSortu();
 int JOKOA_HilketatxtIrudiaSortu();
 int JOKOA_Hilketatxt1IrudiaSortu();
-int JOKOA_kultoatxt3IrudiaSortu();
 int JOKOA_AitaIrudiaSortu();
-int JOKOA_hilketaIrudiaSortu();
+int JOKOA_Aita1IrudiaSortu();
 int JOKOA_hilketatxtIrudiaSortu();
 int JOKOA_hilketatxt1IrudiaSortu();
 int JOKOA_hilketatxt2IrudiaSortu();
-int JOKOA_Aita1IrudiaSortu();
-int JOKOA_bulegoaIrudiaSortu();
-int JOKOA_bulegoatxt1IrudiaSortu();
-int JOKOA_bulegoatxt2IrudiaSortu();
 int JOKOA_bulegoatxt3IrudiaSortu();
 int JOKOA_bulegoatxt4IrudiaSortu();
 int JOKOA_bulegoatxt5IrudiaSortu();
 int JOKOA_bulegoatxt6IrudiaSortu();
-int JOKOA_galduIrudiaSortu();
-int JOKOA_galdu1IrudiaSortu();
-int JOKOA_kultoaIrudiaSortu();
-int JOKOA_MapaIrudiaSortu();
-int JOKOA_Mapa1IrudiaSortu();
-void kopiatuMapaNagusia();
-void kopiatuMapaNagusia1();
-void kopiatuMapaBulegoa();
-void kopiatuMapaKultoa();
-void kopiatuMapaHilketa();
-int PUZZLE1_irudiaBistaratu(); //puzzle1
-int seleccion1();
-int seleccion2();
-int PUZZLE_irudiaBistaratu(); //puzzle3
+int JOKOA_ErrekaIrudiaSortu();
+int JOKOA_kultoatxt3IrudiaSortu();
 int JOKOA_Aita2IrudiaSortu();
-void kopiatuMapaNagusia2();
 int JOKOApanpinaIrudiaSortu();
 int lehenElkarrizketa();
-int mainMenu();
-int mugimendua(int puzzle6, int kulto);
-void hilketatikMaparaAtera();
 void hilketaTextua();
-int lehenengoPuzzlea();
-void bulegoLiburutegia();
 void bulegoTextSortu();
-void elkarrizketaEzDagoEginda();
-void elkarrizketaEgindaDago();
-void bulegokoIrudiaSortu();
-void hilketaIrudiaSortu();
-int errekaPuzzlea();
-int JOKOA_notaIrudiaSortu();
-void notaImg();
-int puzzle3(arropa);
-void kultotikMaparaAtera();
 void kultotxt1();
 void kultotxt2();
 void kultotxt3();
 void kultotxt4();
-void arropaJarri();
-void mapaFinala();
-int pausaMenua();
-int amaieraMenua();
-
-//irabazi baduzu
-int irabaztean();
-
-//elkarrizketak
 int elkarrizketaMaltzurrarekin(int id);
+void elkarrizketaEzDagoEginda();
+void arropaJarri();
 
-//puntuaketaren elementuak
-int JOKOA_notaIrudiaSortu();
+//Puzzle1
+int PUZZLE1_irudiaBistaratu();
+int lehenengoPuzzlea();
 
-//menuak
-int menuJarraitu();
-int menuIrten2();
-int menuBueltatu();
-int menuIrten();
-
-//puzzle2
+//Puzzle2
+int errekaPuzzlea();
 int PUZZLE3_irudiaBistaratu();
 int seleccion();
 int selec1();
@@ -135,5 +104,43 @@ int pieza9L();
 int pieza9D();
 int pieza9R();
 int pieza9U();
+
+//Puzzle3
+int PUZZLE_irudiaBistaratu();
+int seleccion1();
+int seleccion2();
+int puzzle3(arropa);
+
+//Puzzle4
+void mapaFinala();
+int PUZZLE6_irudiaBistaratu();
+
+//Mapak
+int JOKOA_hilketaIrudiaSortu();
+int JOKOA_bulegoaIrudiaSortu();
+int JOKOA_kultoaIrudiaSortu();
+int JOKOA_MapaIrudiaSortu();
+int JOKOA_Mapa1IrudiaSortu();
+void hilketatikMaparaAtera();
+void bulegokoIrudiaSortu();
+void hilketaIrudiaSortu();
+void kultotikMaparaAtera();
+void elkarrizketaEgindaDago();
+
+//Matrizeak
+void kopiatuMapaNagusia();
+void kopiatuMapaNagusia1();
+void kopiatuMapaBulegoa();
+void kopiatuMapaKultoa();
+void kopiatuMapaHilketa();
+void kopiatuMapaNagusia2();
+
+//Kultoko dokumentuak
+int JOKOA_notaIrudiaSortu();
+void notaImg();
+
+//irabazi baduzu
+int irabaztean();
+
 #endif
 
